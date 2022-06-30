ed2b43b6f1b40a210d2d22d1e7848eda4e908521
shubham <dubeys7151@gmail.com>
2022-06-30 16:48:08 -0400

    added hive.py file for hive related opertaions

---
+++ src/hive.py
@@ -0,0 +1,8 @@
+from pyhive import hive
+
+def connection_setup(host_var,port_var,username_var):    
+    return hive.Connection(host=host_var,port=port_var,username=username_var)  
+
+def create_Cursor (connection):
+    return connection.cursor()
+
