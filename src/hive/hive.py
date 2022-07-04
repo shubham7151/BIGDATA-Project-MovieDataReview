6a8fbd073f7ca871f6fed5227b7b9679b0629c1e
shubham <dubeys7151@gmail.com>
2022-07-03 23:04:12 -0400

    refactor preprocessing and hive file

--- src/hive/hive.py
+++ src/hive/hive.py
@@ -3,6 +3,5 @@ from pyhive import hive
 def connection_setup(host_var,port_var,username_var):    
     return hive.Connection(host=host_var,port=port_var,username=username_var)  
 
-def create_Cursor (connection):
+def create_cursor (connection):
     return connection.cursor()
-
--- src/preprocessing/preprocessing.py
+++ src/preprocessing/preprocessing.py
@@ -1,26 +1,3 @@
-# import time 
-
-# raw_data = open('../data/input/Dataset_movie.txt')
-# data_array= raw_data.readlines()
-# csv_data_format = []
-# for data in data_array:
-#     if not data.isspace():
-#         csv_data_format.append( data.split(',') )
-
-# print("data csv format")
-# print(csv_data_format[-1])
-
-
-# print("time")
-
-
-# print(time.strftime('%H:%M:%S',time.gmtime(float(csv_data_format[1][-1]))))
-
-# # csv_data =[]
-# # for data in data_array:
-# #     data
-
-# # 
 #!/usr/bin/env python
 
 
