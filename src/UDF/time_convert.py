3283fcbd539b68a96b12849e775a0adc2ad7e207
shubham <dubeys7151@gmail.com>
2022-07-05 05:42:25 -0400

    improved exception handling to print exception

--- src/UDF/time_convert.py
+++ src/UDF/time_convert.py
@@ -7,5 +7,5 @@ try:
         line = line.strip('\n\r')
         temp = line.split('\t')
         print(str(temp[0])+" "+str(temp[1])+" "+str(temp[2])+" "+str(temp[3])+" "+str(time.strftime('%H:%M:%S',time.gmtime(float(temp[4])))))
-except:
-    print(sys.stderr.errors)
+except Exception as e:
+    print(e)
