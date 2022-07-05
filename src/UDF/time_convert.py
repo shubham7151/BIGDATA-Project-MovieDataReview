afcfd9beb4042e79dffa7e340d5fe53db62b6bea
shubham <dubeys7151@gmail.com>
2022-07-05 02:49:25 -0400

    added new UDF to convert sec into time with format (HH:MM:SS)

---
+++ src/UDF/time_convert.py
@@ -0,0 +1,11 @@
+import sys
+import time
+try:
+    lines = sys.stdin.readlines()
+    x = []
+    for line in lines:
+        line = line.strip('\n\r')
+        temp = line.split('\t')
+        print(str(temp[0])+" "+str(temp[1])+" "+str(temp[2])+" "+str(temp[3])+" "+str(time.strftime('%H:%M:%S',time.gmtime(float(temp[4])))))
+except:
+    print(sys.stderr.errors)
