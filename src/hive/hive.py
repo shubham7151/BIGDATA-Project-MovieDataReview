0b460d09f1a44acc6456b70c98ce5a6145925dbb
shubham <dubeys7151@gmail.com>
2022-07-05 05:45:29 -0400

    added new function to drop table from hive

--- src/hive/hive.py
+++ src/hive/hive.py
@@ -5,3 +5,10 @@ def connection_setup(host_var,port_var,username_var):
 
 def create_cursor (connection):
     return connection.cursor()
+
+def drop_table(cur, db_name ,table_name):
+    try:
+        cur.execute('drop table' + db_name +'.'+table_name )
+        print("table dropped successful")
+    except Exception as e:
+        print(e)
